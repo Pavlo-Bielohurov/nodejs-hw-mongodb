47f6fc346e7a97620e3ef0ca9e2c2a5ce8798a66
Pavlo-Bielohurov <158511567+Pavlo-Bielohurov@users.noreply.github.com>
2024-11-14 22:49:06 +0200

    finish?

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,17 @@
-export const errorHandler = (error, req, res, next) => {
-  const { status = 500, message = 'Something went wrong' } = error;
-  res.status(status).json({
-    status,
-    errorName: error.name,
-    message,
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+      data: err.errors || null,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
   });
 };
--- src/utils/validateBody.js
+++ src/utils/validateBody.js
@@ -3,11 +3,12 @@ import createHttpError from 'http-errors';
 export const validateBody = (schema) => async (req, res, next) => {
   try {
     await schema.validateAsync(req.body, {
-      abortEarly: false,
+      abortEarly: false, 
     });
     next();
   } catch (err) {
-    const error = createHttpError(400, 'Bad request', { errors: err.details });
+    const errorMessages = err.details.map((detail) => detail.message);
+    const error = createHttpError(400, 'Bad request', { errors: errorMessages });
     next(error);
   }
 };
